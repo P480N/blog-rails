4049d83004c04abfc6b3e02e60294efd90dcbf53
Andrés Duarte <andres.duarte.marquez@gmail.com>
2016-11-26 16:54:41 -0300

    Crear y eliminar post  Changes to be committed: 	modified:   app/assets/stylesheets/blog.css 	modified:   app/controllers/posts_controller.rb 	modified:   app/models/post.rb 	new file:   app/views/posts/create.html.erb 	modified:   app/views/posts/index.html.erb 	new file:   app/views/posts/new.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,35 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
