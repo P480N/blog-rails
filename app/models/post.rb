4bf40f0f1b345f946e8812459ff57951b1c9e2f8
Andrés Duarte <andres.duarte.marquez@gmail.com>
2016-11-27 16:56:54 -0300

    Comentarios  Changes to be committed: 	new file:   app/assets/javascripts/comments.coffee 	new file:   app/assets/stylesheets/comments.scss 	new file:   app/controllers/comments_controller.rb 	modified:   app/controllers/posts_controller.rb 	new file:   app/helpers/comments_helper.rb 	new file:   app/models/comment.rb 	modified:   app/models/post.rb 	modified:   app/models/user.rb 	new file:   app/views/comments/_comment.json.jbuilder 	new file:   app/views/comments/_form.html.erb 	new file:   app/views/comments/edit.html.erb 	new file:   app/views/comments/index.html.erb 	new file:   app/views/comments/index.json.jbuilder 	new file:   app/views/comments/new.html.erb 	new file:   app/views/comments/show.html.erb 	new file:   app/views/comments/show.json.jbuilder 	modified:   app/views/posts/show.html.erb 	modified:   config/routes.rb 	new file:   db/migrate/20161127184921_create_comments.rb 	modified:   db/schema.rb 	new file:   test/controllers/comments_controller_test.rb 	new file:   test/fixtures/comments.yml 	new file:   test/models/comment_test.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
+	has_many :comments
 	validates :title, presence: true, uniqueness: true
 	validates :body, presence: true, length: {minimum: 20}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-
+  
   devise_for :users
-  resources :posts
-
+  
+  resources :posts do
+    resources :comments, only: [:create, :destroy, :update]
+  end
+  
   get 'welcome/index'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  
   #root 'posts#index'
 
   # Example of regular route:
