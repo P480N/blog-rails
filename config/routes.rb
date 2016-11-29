b9ad048636cf66b074cdae1af681cc43b0402117
Andrés Duarte M <andres.duarte.marquez@gmail.com>
2016-11-29 12:38:10 -0300

    Rutas comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   
+  resources :comments
+  
   resources :posts do
     resources :comments, only: [:create, :destroy, :update, :show]
   end
