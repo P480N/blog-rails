a1137983b20659ed7f9194c2508a619ae73e498b
Andrés Duarte M <andres.duarte.marquez@gmail.com>
2016-11-29 16:55:55 -0300

    Se integra paperclip para subir la portada del post  Cambios para hacer commit: 	modificado: app/controllers/posts_controller.rb 	modificado: app/models/post.rb 	modificado: app/views/posts/_form.html.erb 	modificado: app/views/posts/show.html.erb 	new file:   db/migrate/20161129192941_add_cover_to_posts.rb 	modificado: db/schema.rb 	new file:   public/system/posts/covers/000/000/002/medium/Goku_DB_Artwork.png 	new file:   public/system/posts/covers/000/000/002/original/Goku_DB_Artwork.png 	new file:   public/system/posts/covers/000/000/002/thumb/Goku_DB_Artwork.png

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161127184921) do
+ActiveRecord::Schema.define(version: 20161129192941) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -25,11 +25,15 @@ ActiveRecord::Schema.define(version: 20161127184921) do
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "body",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "user_id",    limit: 4
+    t.string   "title",              limit: 255
+    t.text     "body",               limit: 65535
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.integer  "user_id",            limit: 4
+    t.string   "cover_file_name",    limit: 255
+    t.string   "cover_content_type", limit: 255
+    t.integer  "cover_file_size",    limit: 4
+    t.datetime "cover_updated_at"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
